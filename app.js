32591e2b34f8aa4c6f9022a9490a62943c4dfdcb
Bolaji Ayodele <ayodelej30@gmail.com>
2023-03-23 15:25:29 -0600

    first day

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 //All your code goes here
+
+app.get("/courses", (req, res) => {
+  let courses = JSON.parse(
+    fs.readFileSync(__dirname + "/database/courses.json")
+  );
+  res.send(courses);
+});
+
+app.get("/account/:id", (req, res) => {
+  console.log(req.params.id);
+});
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
